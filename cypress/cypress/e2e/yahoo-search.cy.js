f5b818cc209eacbfbbe79d037805b2da40532850
Yan-Exe <yanpatrick199@gmail.com>
2025-08-05 21:40:30 -0300

    adicionar a busca por 'pacto soluções' no yahoo

---
+++ cypress/cypress/e2e/yahoo-search.cy.js
@@ -0,0 +1,7 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://br.search.yahoo.com/?fr2=p:fprd,mkt:br')
+
+    cy.get('input[name="p"').type('Pacto Soluções{enter}');
+  })
+})  
