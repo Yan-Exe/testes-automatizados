d42e88948027be1d3766cf93b2aeb2c8cd1e7c2e
Yan-Exe <yanpatrick199@gmail.com>
2025-08-06 22:46:28 -0300

    commit final

--- cypress/cypress/e2e/uol-validation.cy.js
+++ cypress/cypress/e2e/uol-validation.cy.js
@@ -1,5 +1,13 @@
-describe('Teste UOL com cy.origin', () => {
-  it('Valida texto Atualização na página externa', () => {
+// Teste automatizado para validar a data de atualização dos termos de segurança no UOL
+
+// Primeiro é efetuado o acesso ao site do UOL
+// Onde é localizado o link 'Segurança e privacidade' e clicado
+// Após, é aguardado o carregamento da página externa
+// Logo, é procurado o texto específico de atualização
+// Onde então o parágrafo é validado e a data salva no log
+
+describe('Valida data de atualização de termos no UOL', () => {
+  it('Validado a data de atualização', () => {
     cy.visit('https://www.uol.com.br/');
 
     cy.contains('a', 'Segurança e privacidade').click();
@@ -7,7 +15,10 @@ describe('Teste UOL com cy.origin', () => {
     cy.on('uncaught:exception', () => false);
 
     cy.origin('https://sobreuol.noticias.uol.com.br', () => {
-      cy.contains('p', 'Atualização:').should('be.visible');
+      cy.contains('p', 'Atualização:').then(data => {
+          const dataAtualizacao = data.text();
+          cy.log('Data de atualização: ' + dataAtualizacao);
+      })
     });
   });
 });
--- cypress/cypress/e2e/yahoo-search.cy.js
+++ cypress/cypress/e2e/yahoo-search.cy.js
@@ -1,9 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
+// Teste automatizado para validar a busca no Yahoo
+
+// Primeiramento é efetuado ocesso o endereço do Yahoo
+// Em seguida, no campo de busca e digita-se "Pacto Soluções" e apertado 'Enter'.
+// Onde então é validado se houve um resultado adequado
+
+describe('Validação de busca no Yahoo', () => {
+  it('Deve exibir resultados adequados na busca', () => {
     cy.visit('https://br.search.yahoo.com/?fr2=p:fprd,mkt:br')
 
     cy.get('input[name="p"').type('Pacto Soluções{enter}');
 
     cy.contains('Pacto Sistema para Academias').should('be.visible');
+    cy.log("Busca sucessida")
   })
 })  
